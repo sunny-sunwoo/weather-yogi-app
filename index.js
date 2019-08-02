b91541bac7fd8097ac706ae8855cd01edf08c996
sunny-sunwoo <sunny.sunwoo.park@gmail.com>
2019-08-01 19:35:50 -0700

    changing port

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const Datastore = require('nedb');
 const fetch = require('node-fetch');
 require('dotenv').config();
-const app = express();
 
-app.listen(3000, () => { console.log('listening at port 3000') });
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Starting server at ${port}`);
+});
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
+
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
